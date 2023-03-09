fd813ec64582e0272e4226339337e7e432df7d05
奇葩の灵梦 <415551921@qq.com>
2023-03-10 02:47:20 +0800

    Initial commit

---
+++ src/main/java/net/cutereimu/Bitset.java
@@ -0,0 +1,226 @@
+package net.cutereimu;
+
+import java.util.Arrays;
+
+public final class Bitset {
+    private int numBits;
+    private byte[] bits;
+
+    private Bitset(int numBits, byte[] bits) {
+        this.numBits = numBits;
+        this.bits = bits;
+    }
+
+    public Bitset(boolean... v) {
+        bits = new byte[0];
+        append(v);
+    }
+
+    public Bitset(Bitset obj) {
+        numBits = obj.numBits;
+        bits = obj.bits;
+    }
+
+    public Bitset substr(int start, int end) {
+        if (start > end || end > numBits) {
+            throw new IllegalArgumentException("Out of range start=%d end=%d numBits=%d".formatted(start, end, numBits));
+        }
+
+        Bitset result = new Bitset();
+        result.ensureCapacity(end - start);
+
+        for (int i = start; i < end; i++) {
+            if (at(i)) {
+                result.bits[result.numBits / 8] |= 0x80 >>> (result.numBits % 8);
+            }
+            result.numBits++;
+        }
+
+        return result;
+    }
+
+    public static Bitset fromString(String s) {
+        Bitset b = new Bitset(0, new byte[0]);
+
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '1':
+                    b.append(true);
+                    break;
+                case '0':
+                    b.append(false);
+                    break;
+                case ' ':
+                    break;
+                default:
+                    throw new RuntimeException("Invalid char %c in NewFromBase2String".formatted(c));
+            }
+        }
+
+        return b;
+    }
+
+    public void append(byte[] data) {
+        for (byte d : data) {
+            append(d, 8);
+        }
+    }
+
+    public void append(byte value, int numBits) {
+        ensureCapacity(numBits);
+
+        if (numBits > 8) {
+            throw new IllegalArgumentException("numBits %d out of range 0-8".formatted(numBits));
+        }
+
+        for (int i = numBits - 1; i >= 0; i--) {
+            if ((value & (1 << i)) != 0) {
+                bits[this.numBits / 8] |= 0x80 >>> (this.numBits % 8);
+            }
+
+            this.numBits++;
+        }
+    }
+
+    public void append(int value, int numBits) {
+        ensureCapacity(numBits);
+
+        if (numBits > 32) {
+            throw new IllegalArgumentException("numBits %d out of range 0-32".formatted(numBits));
+        }
+
+        for (int i = numBits - 1; i >= 0; i--) {
+            if ((value & (1 << i)) != 0) {
+                bits[this.numBits / 8] |= 0x80 >>> (this.numBits % 8);
+            }
+
+            this.numBits++;
+        }
+    }
+
+    private void ensureCapacity(int numBits) {
+        numBits += this.numBits;
+
+        int newNumBits = numBits / 8;
+        if (numBits % 8 != 0) {
+            newNumBits++;
+        }
+
+        if (bits.length >= newNumBits) {
+            return;
+        }
+
+        bits = Arrays.copyOfRange(bits, 0, bits.length * 3 + newNumBits);
+    }
+
+    public void append(Bitset other) {
+        ensureCapacity(other.numBits);
+
+        for (int i = 0; i < other.numBits; i++) {
+            if (other.at(i)) {
+                bits[numBits / 8] |= 0x80 >>> (numBits % 8);
+            }
+            numBits++;
+        }
+    }
+
+    public void append(boolean... bits) {
+        ensureCapacity(bits.length);
+
+        for (boolean v : bits) {
+            if (v) {
+                this.bits[numBits / 8] |= 0x80 >>> numBits % 8;
+            }
+            numBits++;
+        }
+    }
+
+    public void append(int num, boolean value) {
+        for (int i = 0; i < num; i++) {
+            append(value);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("numBits=%d, bits=".formatted(numBits));
+        for (int i = 0; i < numBits; i++) {
+            if ((i % 8) == 0) {
+                sb.append(' ');
+            }
+
+            if ((bits[i / 8] & (0x80 >>> (i % 8))) != 0) {
+                sb.append('1');
+            } else {
+                sb.append('0');
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public int size() {
+        return numBits;
+    }
+
+    public boolean[] bits() {
+        boolean[] result = new boolean[numBits];
+
+        for (int i = 0; i < numBits; i++) {
+            result[i] = (bits[i / 8] & (0x80 >>> (i % 8))) != 0;
+        }
+
+        return result;
+    }
+
+    public boolean at(int index) {
+        if (index >= numBits) {
+            throw new IllegalArgumentException("Index %d out of range".formatted(index));
+        }
+
+        return (bits[index / 8] & (0x80 >>> (index % 8))) != 0;
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (this == otherObject) return true;
+        if (!(otherObject instanceof Bitset other)) return false;
+
+        if (numBits != other.numBits)
+            return false;
+
+        for (int i = 0; i < numBits / 8; i++) {
+            if (bits[i] != other.bits[i]) {
+                return false;
+            }
+        }
+
+        for (int i = 8 * (numBits / 8); i < numBits; i++) {
+            byte a = (byte) (bits[i / 8] & (0x80 >>> (i % 8)));
+            byte b = (byte) (other.bits[i / 8] & (0x80 >>> (i % 8)));
+
+            if (a != b) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public byte byteAt(int index) {
+        if (index < 0 || index >= numBits) {
+            throw new IllegalArgumentException("Index %d out of range".formatted(index));
+        }
+
+        byte result = 0;
+
+        for (int i = index; i < index + 8 && i < numBits; i++) {
+            result <<= 1;
+            if (at(i)) {
+                result |= 1;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/BitsetTest.java
@@ -0,0 +1,216 @@
+import net.cutereimu.Bitset;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class BitsetTest {
+    private static final boolean b0 = false;
+    private static final boolean b1 = true;
+
+    @Test
+    public void testNewBitset() {
+        boolean[][] tests = {
+                {},
+                {b1},
+                {b0},
+                {b1, b0},
+                {b1, b0, b1},
+                {b0, b0, b1}
+        };
+
+        for (boolean[] v : tests) {
+            Bitset result = new Bitset(v);
+            Assertions.assertArrayEquals(result.bits(), v);
+        }
+    }
+
+    @Test
+    public void testAppend() {
+        boolean[] randomBooleans = new boolean[128];
+
+        Random rng = ThreadLocalRandom.current();
+
+        for (int i = 0; i < randomBooleans.length; i++) {
+            randomBooleans[i] = rng.nextBoolean();
+        }
+
+        for (int i = 0; i < randomBooleans.length - 1; i++) {
+            Bitset a = new Bitset(Arrays.copyOfRange(randomBooleans, 0, i));
+            Bitset b = new Bitset(Arrays.copyOfRange(randomBooleans, i, randomBooleans.length));
+
+            a.append(b);
+
+            Assertions.assertArrayEquals(a.bits(), randomBooleans);
+        }
+    }
+
+    private static class TestData<T> {
+        public Bitset initial;
+        public T value;
+        public int numBits;
+        public Bitset expected;
+
+        public TestData(Bitset initial, T value, int numBits, Bitset expected) {
+            this.initial = initial;
+            this.value = value;
+            this.numBits = numBits;
+            this.expected = expected;
+        }
+    }
+
+    @Test
+    public void testAppendByte() {
+        List<TestData<Byte>> tests = new ArrayList<>();
+        tests.add(new TestData<>(new Bitset(), (byte) 0x01, 1, new Bitset(b1)));
+        tests.add(new TestData<>(new Bitset(b1), (byte) 0x01, 1, new Bitset(b1, b1)));
+        tests.add(new TestData<>(new Bitset(b0), (byte) 0x01, 1, new Bitset(b0, b1)));
+        tests.add(new TestData<>(new Bitset(b1, b0, b1, b0, b1, b0, b1), (byte) 0xAA, 2, new Bitset(b1, b0, b1, b0, b1, b0, b1, b1, b0)));
+        tests.add(new TestData<>(new Bitset(b1, b0, b1, b0, b1, b0, b1), (byte) 0xAA, 8, new Bitset(b1, b0, b1, b0, b1, b0, b1, b1, b0, b1, b0, b1, b0, b1, b0)));
+
+        for (TestData<Byte> test : tests) {
+            test.initial.append(test.value, test.numBits);
+            Assertions.assertArrayEquals(test.initial.bits(), test.expected.bits());
+        }
+    }
+
+    @Test
+    public void testAppendInt() {
+        List<TestData<Integer>> tests = new ArrayList<>();
+        tests.add(new TestData<>(new Bitset(), 0xAAAAAAAF, 4, new Bitset(b1, b1, b1, b1)));
+        tests.add(new TestData<>(new Bitset(), 0xFFFFFFFF, 32,
+                new Bitset(b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1, b1)));
+        tests.add(new TestData<>(new Bitset(), 0x0, 32,
+                new Bitset(b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0)));
+        tests.add(new TestData<>(new Bitset(), 0xAAAAAAAA, 32,
+                new Bitset(b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0)));
+        tests.add(new TestData<>(new Bitset(), 0xAAAAAAAA, 31,
+                new Bitset(b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0, b1, b0)));
+
+        for (TestData<Integer> test : tests) {
+            test.initial.append(test.value, test.numBits);
+            Assertions.assertArrayEquals(test.initial.bits(), test.expected.bits());
+        }
+    }
+
+    @Test
+    public void testAppendBooleans() {
+        boolean[] randomBooleans = new boolean[128];
+
+        Random rng = ThreadLocalRandom.current();
+
+        for (int i = 0; i < randomBooleans.length; i++) {
+            randomBooleans[i] = rng.nextBoolean();
+        }
+
+        for (int i = 0; i < randomBooleans.length - 1; i++) {
+            Bitset result = new Bitset(Arrays.copyOfRange(randomBooleans, 0, i));
+            result.append(new Bitset(Arrays.copyOfRange(randomBooleans, i, randomBooleans.length)));
+
+            Assertions.assertArrayEquals(result.bits(), randomBooleans);
+        }
+    }
+
+    @Test
+    public void testSize() {
+        boolean[] randomBooleans = new boolean[128];
+
+        Random rng = ThreadLocalRandom.current();
+
+        for (int i = 0; i < randomBooleans.length; i++) {
+            randomBooleans[i] = rng.nextBoolean();
+        }
+
+        for (int i = 0; i < randomBooleans.length - 1; i++) {
+            Bitset result = new Bitset(Arrays.copyOfRange(randomBooleans, 0, i));
+
+            Assertions.assertEquals(result.size(), i);
+        }
+    }
+
+    @Test
+    public void testAt() {
+        boolean[] test = {b0, b1, b0, b1, b0, b1, b1, b0, b1};
+
+        Bitset bitset = new Bitset(test);
+        for (int i = 0; i < test.length; i++) {
+            boolean result = bitset.at(i);
+
+            Assertions.assertEquals(result, test[i]);
+        }
+    }
+
+    @Test
+    public void test() {
+        Bitset b = new Bitset();
+        Assertions.assertArrayEquals(b.bits(), new boolean[0]);
+
+        b.append(b1, b1, b0);
+        Assertions.assertArrayEquals(b.bits(), new boolean[]{b1, b1, b0});
+
+        b.append(b1);
+        Assertions.assertArrayEquals(b.bits(), new boolean[]{b1, b1, b0, b1});
+
+        b.append((byte) 0x02, 4);
+        Assertions.assertArrayEquals(b.bits(), new boolean[]{b1, b1, b0, b1, b0, b0, b1, b0});
+    }
+
+    @Test
+    public void testByteAt() {
+        boolean[] data = {b0, b1, b0, b1, b0, b1, b1, b0, b1};
+
+        Map<Integer, Byte> tests = new HashMap<>();
+        tests.put(0, (byte) 0x56);
+        tests.put(1, (byte) 0xad);
+        tests.put(2, (byte) 0x2d);
+        tests.put(5, (byte) 0x0d);
+        tests.put(8, (byte) 0x01);
+
+        for (Entry<Integer, Byte> test : tests.entrySet()) {
+            Bitset b = new Bitset();
+            b.append(data);
+
+            byte result = b.byteAt(test.getKey());
+
+            Assertions.assertEquals(result, test.getValue());
+        }
+    }
+
+    private static class TestSubstrData {
+        public int start;
+        public int end;
+        public boolean[] expected;
+
+        public TestSubstrData(int start, int end, boolean[] expected) {
+            this.start = start;
+            this.end = end;
+            this.expected = expected;
+        }
+    }
+
+    @Test
+    public void testSubstr() {
+        boolean[] data = {b0, b1, b0, b1, b0, b1, b1, b0};
+
+        TestSubstrData[] tests = {
+                new TestSubstrData(0, 8, new boolean[]{b0, b1, b0, b1, b0, b1, b1, b0}),
+                new TestSubstrData(0, 0, new boolean[0]),
+                new TestSubstrData(0, 1, new boolean[]{b0}),
+                new TestSubstrData(2, 4, new boolean[]{b0, b1})
+        };
+
+        for (TestSubstrData test : tests) {
+            Bitset b = new Bitset();
+            b.append(data);
+
+            Bitset result = b.substr(test.start, test.end);
+
+            Bitset expected = new Bitset();
+            expected.append(test.expected);
+
+            Assertions.assertEquals(result, expected);
+        }
+    }
+}
